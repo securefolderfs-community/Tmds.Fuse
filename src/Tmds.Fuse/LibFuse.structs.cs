04bdba71181b1bc40e86b9a5aa848c18e54c73ff
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-01-08 19:18:04 +0100

    Fix method signatures

--- src/Tmds.Fuse/LibFuse.structs.cs
+++ src/Tmds.Fuse/LibFuse.structs.cs
@@ -44,7 +44,7 @@ namespace Tmds.Fuse
     unsafe delegate int chown_delegate(path* path, uint uid, uint gid, fuse_file_info* fi);
     unsafe delegate int statfs_delegate(path* path, statvfs* vfs);
     unsafe delegate int flush_delegate(path* path, fuse_file_info* fi);
-    unsafe delegate int fsync_delegate(path* path, fuse_file_info* fi);
+    unsafe delegate int fsync_delegate(path* path, int datasync, fuse_file_info* fi);
     unsafe delegate int setxattr_delegate(path* path, void* name, void* buffer, size_t size, int flags);
     unsafe delegate int getxattr_delegate(path* path, void* name, void* buffer, size_t size);
     unsafe delegate int listxattr_delegate(path* path, void* buffer, size_t size);
@@ -202,7 +202,7 @@ namespace Tmds.Fuse
         *  - When writeback caching is disabled, the filesystem is
         *    expected to properly handle the O_APPEND flag and ensure
         *    that each write is appending to the end of the file.
-        * 
+        *
             *  - When writeback caching is enabled, the kernel will
         *    handle O_APPEND. However, unless all changes to the file
         *    come through the kernel this will not work reliably. The
